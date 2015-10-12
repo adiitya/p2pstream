3d909d3937082b8183446cec89defa5d32f7d129
Aditya Prakash <adiitya.adda@gmail.com>
2015-10-12 17:50:46 +0530

    Allows server to send data

--- server/source/server.cpp
+++ server/source/server.cpp
@@ -56,8 +56,8 @@ void* Server::respondToClient(void* clientSock)
 	char name[100] = {0};
 	Data* dataRec = new BufferData(name, 100);
 	clientConn.receiveData(*dataRec);
-	//Data* dataSend = new FileData(name, FileData::TYPE::READ);
-	//clientConn.sendData(*dataSend);
+	Data* dataSend = new FileData(name, FileData::TYPE::READ);
+	clientConn.sendData(*dataSend);
 
 	printf("Message sent to client: %d \n", *((int*)clientSock));
 
